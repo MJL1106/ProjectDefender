742907ceee009cf7b0f02cfc61a056d407459ec8
MJL1106 <matthew.lowe2468@gmail.com>
2025-08-29 14:43:31 +0100

    Improved rotation smoothness

--- ProjectDefender/Assets/Enemy.cs
+++ ProjectDefender/Assets/Enemy.cs
@@ -5,22 +5,38 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent agent;
-    
+
+    [SerializeField] private float turnSpeed = 10;
     [SerializeField] private Transform[] waypoint;
     private int waypointIndex;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
     }
 
     private void Update()
     {
+        FaceTarget(agent.steeringTarget);
+        
         // Check if the agent is close to current target point
         if (agent.remainingDistance < .5f)
         {
             agent.SetDestination(GetNextWaypoint());
         }
+    }
+
+    private void FaceTarget(Vector3 newTarget)
+    {
+        Vector3 directionToTarget = newTarget - transform.position;
+        if (directionToTarget.magnitude == 0) return;
+        directionToTarget.y = 0;
+
+        // Create a rotation that points the forward vector to the calculated direction
+        Quaternion newRotation = Quaternion.LookRotation(directionToTarget);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, turnSpeed * Time.deltaTime);
     }
 
     private Vector3 GetNextWaypoint()
