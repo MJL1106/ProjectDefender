c75032f27f320d80ae4f106e3af64a2fe6554ad6
MJL1106 <matthew.lowe2468@gmail.com>
2025-08-30 19:53:00 +0100

    Setup tower string and rotor animation
    Setup rotor on the tower to move to visualise a shooting animation.

--- ProjectDefender/Assets/Scripts/Player/Crossbow/CrossbowVisuals.cs
+++ ProjectDefender/Assets/Scripts/Player/Crossbow/CrossbowVisuals.cs
@@ -1,19 +1,21 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CrossbowVisuals : MonoBehaviour
 {
    private TowerCrossbow myTower;
-   
+
    [SerializeField] private LineRenderer attackVisuals;
    [SerializeField] private float attackVisualDuration = .1f;
 
-   [Header("Glowing Visuals")]
-   [SerializeField] private MeshRenderer meshRenderer;
+   [Header("Glowing Visuals")] [SerializeField]
+   private MeshRenderer meshRenderer;
+
    private Material material;
-   
-   [Space]
+
+   [Space] 
    private float currentIntensity;
    [SerializeField] private float maxIntensity = 150;
 
@@ -21,21 +23,65 @@ public class CrossbowVisuals : MonoBehaviour
    [SerializeField] private Color startColor;
    [SerializeField] private Color endColor;
 
+   [Header("Rotor Visuals")] 
+   [SerializeField] private Transform rotor;
+   [FormerlySerializedAs("unloaded")] [SerializeField] private Transform rotorUnloaded;
+   [FormerlySerializedAs("loaded")] [SerializeField] private Transform rotorLoaded;
 
-   private void Awake()
+   [Header("Front Glow String")] 
+   [SerializeField] private LineRenderer frontStringL;
+   [SerializeField] private LineRenderer frontStringR;
+
+   [Space] 
+   [SerializeField] private Transform frontStartPointL;
+   [SerializeField] private Transform frontStartPointR;
+   [SerializeField] private Transform frontEndPointL;
+   [SerializeField] private Transform frontEndPointR;
+
+   [Header("Back Glow String")] 
+   [SerializeField] private LineRenderer backStringL;
+   [SerializeField] private LineRenderer backStringR;
+   
+   [Space] 
+   [SerializeField] private Transform backStartPointL;
+   [SerializeField] private Transform backStartPointR;
+   [SerializeField] private Transform backEndPointL;
+   [SerializeField] private Transform backEndPointR;
+
+   [SerializeField] private LineRenderer[] lineRenderers;
+
+
+private void Awake()
    {
       myTower = GetComponent<TowerCrossbow>();
-
       material = new Material(meshRenderer.material);
-
       meshRenderer.material = material;
 
+      UpdateMaterialsOnLineRenderers();
+      
       StartCoroutine(ChangeEmission(1));
+   }
+
+   private void UpdateMaterialsOnLineRenderers()
+   {
+      foreach (var lr in lineRenderers)
+      {
+         lr.material = material;
+      }
    }
 
    private void Update()
    {
       UpdateEmissionColor();
+      UpdateStrings();
+   }
+
+   private void UpdateStrings()
+   {
+      UpdateStringVisual(frontStringL, frontStartPointL, frontEndPointL);
+      UpdateStringVisual(frontStringR, frontStartPointR, frontEndPointR);
+      UpdateStringVisual(backStringL, backStartPointL, backEndPointL);
+      UpdateStringVisual(backStringR, backStartPointR, backEndPointR);
    }
 
    private void UpdateEmissionColor()
@@ -47,7 +93,10 @@ public class CrossbowVisuals : MonoBehaviour
 
    public void PlayerReloadFX(float duration)
    {
-      StartCoroutine(ChangeEmission(duration / 2));
+      float newDuration = duration / 2;
+      
+      StartCoroutine(ChangeEmission(newDuration));
+      StartCoroutine(UpdateRotorPosition(newDuration));
    }
 
    public void PlayAttackVFX(Vector3 startPoint, Vector3 endPoint)
@@ -84,5 +133,25 @@ public class CrossbowVisuals : MonoBehaviour
       }
 
       currentIntensity = maxIntensity;
+   }
+
+   private IEnumerator UpdateRotorPosition(float duration)
+   {
+      float startTime = Time.time;
+
+      while (Time.time - startTime < duration)
+      {
+         float tValue = (Time.time - startTime) / duration;
+         rotor.position = Vector3.Lerp(rotorUnloaded.position, rotorLoaded.position, tValue);
+         yield return null;
+      }
+
+      rotor.position = rotorLoaded.position;
+   }
+
+   private void UpdateStringVisual(LineRenderer lineRenderer, Transform startPoint, Transform endPoint)
+   {
+      lineRenderer.SetPosition(0, startPoint.position);
+      lineRenderer.SetPosition(1, endPoint.position);
    }
 }
