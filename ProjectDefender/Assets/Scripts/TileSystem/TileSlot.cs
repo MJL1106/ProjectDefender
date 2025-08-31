689a4ebff5e53e43bcd7beeb12d1ad3bb1346e21
MJL1106 <matthew.lowe2468@gmail.com>
2025-08-31 20:04:53 +0100

    Setup buttons to switch assets in scene
    Created a script so you can click on assets e.g. the field and switch it to a sideway in the click of a button

--- ProjectDefender/Assets/Scripts/TileSystem/TileSlot.cs
+++ ProjectDefender/Assets/Scripts/TileSystem/TileSlot.cs
@@ -1,12 +1,42 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class TileSlot : MonoBehaviour
 {
-    public bool canBuild;
-
-    public void ButtonCheck()
+    private MeshRenderer meshRenderer => GetComponent<MeshRenderer>();
+    private MeshFilter meshFilter => GetComponent<MeshFilter>();
+    public void SwitchTile(GameObject referenceTile)
     {
-        canBuild = !canBuild;
-        Debug.Log("haha");
+        TileSlot newTile = referenceTile.GetComponent<TileSlot>();
+
+        meshFilter.mesh = newTile.GetMesh();
+        meshRenderer.material = newTile.GetMaterial();
+
+        foreach (GameObject obj in GetAllChildren())
+        {
+            DestroyImmediate(obj);
+        }
+
+        foreach (GameObject obj in newTile.GetAllChildren())
+        {
+            Instantiate(obj, transform);
+        }
+    }
+
+    public Material GetMaterial() { return meshRenderer.sharedMaterial; }
+    
+    public Mesh GetMesh() { return meshFilter.sharedMesh; }
+
+    public List<GameObject> GetAllChildren()
+    {
+        List<GameObject> children = new List<GameObject>();
+
+        foreach (Transform child in transform)
+        {
+            children.Add(child.gameObject);
+        }
+
+        return children;
     }
 }
