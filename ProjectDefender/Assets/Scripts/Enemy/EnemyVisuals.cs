d4618ad6bee9aecd3afe7df7f3a517b95c34f1ca
MJL1106 <matthew.lowe2468@gmail.com>
2025-09-02 16:10:37 +0100

    Setup enemy rotation up hills
    Enemy now rotates when moving up a hill to look polished.

---
+++ ProjectDefender/Assets/Scripts/Enemy/EnemyVisuals.cs
@@ -0,0 +1,29 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem.Android;
+
+public class EnemyVisuals : MonoBehaviour
+{
+    [SerializeField] private Transform visuals;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float verticalRotationSpeed;
+
+    private void Update()
+    {
+        AlignWithSlope();
+    }
+
+    private void AlignWithSlope()
+    {
+        if (visuals == null) return;
+
+        if (Physics.Raycast(visuals.position, Vector3.down, out RaycastHit hit, Mathf.Infinity, whatIsGround))
+        {
+            Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+
+            visuals.rotation =
+                Quaternion.Slerp(visuals.rotation, targetRotation, Time.deltaTime * verticalRotationSpeed);
+        }
+    }
+}
