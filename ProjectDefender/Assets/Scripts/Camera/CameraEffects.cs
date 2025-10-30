ca4bfa534b8b62df6a91dcab3b0519efa13894cb
MJL1106 <matthew.lowe2468@gmail.com>
2025-10-30 12:47:40 +0000

    Increased delay on enabling camera movement

--- ProjectDefender/Assets/Scripts/Camera/CameraEffects.cs
+++ ProjectDefender/Assets/Scripts/Camera/CameraEffects.cs
@@ -91,7 +91,7 @@ public class CameraEffects : MonoBehaviour
         cameraCo = StartCoroutine(ChangePositionAndRotation(inGamePosition, inGameRotation, transitionDuration));
         camController.AdjustPitchValue(inGameRotation.eulerAngles.x);
 
-        StartCoroutine(EnableCameraControlsAfter(transitionDuration + .1f));
+        StartCoroutine(EnableCameraControlsAfter(transitionDuration + 1.5f));
     }
 
     public void SwitchToLevelSelectView()
